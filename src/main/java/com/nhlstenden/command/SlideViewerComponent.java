dcb91a3474f3f06ba2b137314f785e02aa078bcf
BramVeninga <bram.veninga@student.nhlstenden.com>
2024-04-05 15:21:38 +0200

    attempted bug fix

--- src/main/java/com/nhlstenden/command/SlideViewerComponent.java
+++ src/main/java/com/nhlstenden/command/SlideViewerComponent.java
@@ -66,7 +66,7 @@ public class SlideViewerComponent
         {
             paintPartOfComponent(graphics);
             Rectangle area = new Rectangle(0, Y_POSITION, this.slide.getDimension("width"), (this.slide.getDimension("height") - Y_POSITION));
-            draw(graphics, area, (ImageObserver) this,);
+            draw(graphics, area, new MyStyle());
         }
     }
 
